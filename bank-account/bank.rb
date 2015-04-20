66b07fb38f0baeac84341e370715228d2d145562
Bryan Summers <bryansummers@Bryans-MacBook-Pro-2.local>
2015-04-20 14:41:44 +1000

    Created Bank class properly

---
+++ bank-account/bank.rb
@@ -0,0 +1,27 @@
+require './user.rb'
+require './account.rb'
+
+class Bank
+  attr_accessor :accounts, :users
+
+  def initialize
+    @accounts = []
+    @users = []
+  end
+
+  def self.find_account(acc_no)
+    @accounts.find { |acc_no| == acc_no }
+  end
+
+  def self.find_user(customer_ref)
+    @users.find { |ref| == customer_ref }
+  end
+end
+
+bank1 = Bank.new
+harry = User.new('Harry', 'Livings', customer_ref)
+bank.users << harry
+acct1 = Account.new(12345)
+acct1.user = harry
+harry.accounts << acct1
+bank.accounts << acct1
